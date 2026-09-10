91b2a4ebacaababd870dcf0415f09f0311bf1327
onahonchloe-byte <onahonchloe@gmail.com>
2026-09-10 10:12:04 +0800

    Added student/books crud

---
+++ database/migrations/003_books_crud.sql
@@ -0,0 +1,44 @@
+SELECT * FROM books;
+
+SELECT * FROM books
+     ORDER BY book_id ASC;
+
+SELECT * FROM books
+     ORDER BY book_id DESC;
+
+SELECT 
+    book_title,
+    book_author
+FROM books
+     ORDER BY book_title ASC;
+
+SELECT 
+    book_title,
+    book_author
+FROM books
+     ORDER BY book_title DESC;
+
+SELECT 
+    book_author,
+    book_title
+FROM books
+     ORDER BY book_author ASC;
+
+SELECT 
+    book_author,
+    book_title
+FROM books
+     ORDER BY book_author DESC;
+
+SELECT 
+    book_title,
+    book_author
+FROM books
+WHERE book_id = 1
+LIMIT 1;
+
+UPDATE books
+SET
+    book_title = 'Harry Potter and the Sorcerer''s Stone',
+    book_author = 'J.K. Rowling'
+WHERE book_id = 1;
